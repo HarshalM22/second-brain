d031d97b20cd82ef15beb04f9d55f22b40dd01ce
HarshalM22 <harshalmankar61@gmail.com>
2025-05-01 01:27:47 +0530

    few changes

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from "react"
+
+interface ButtonProps{
+   variant : "primary" | "secondary" 
+   text : string 
+   startIcon? : ReactElement
+   onClick? :()=>void
+}
+
+const varientClasses = {
+    "primary" : " bg-primary text-white",
+    "secondary" : " bg-secondary text-black ",
+}
+
+const defaultStyles = "px-4 py-2 rounded-md font-normal text-lg flex items-center gap-2";
+export function Button({variant,text,startIcon,onClick}: ButtonProps){
+
+    return <button onClick={onClick} className={varientClasses[variant] + " "+ defaultStyles} >{startIcon}{text}</button>
+}
